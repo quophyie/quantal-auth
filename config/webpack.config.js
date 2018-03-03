0c900bb688d420960714ec17c625189c94929803
quophyie <quophyie@yahoo.com>
2018-03-03 15:01:29 +0000

    Added config.yml Updated the quantal-errors referenced dist/test/**/*tests.js

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -47,12 +47,12 @@ const config = {
     extensions: ['*', '.js', '.jsx', '.json'], /*,
     root: [
       path.resolve('../')
-    ]*/
+    ] */
     modules: [ path.resolve(__dirname, '../'), path.resolve(__dirname, '../node_modules'), 'node_modules'],
     alias: {
       controllers: '../app/controllers',
       enroutenExpress: 'express-enrouten/lib/index.js',
-      rootPath:  path.resolve(__dirname, '../')
+      rootPath: path.resolve(__dirname, '../')
     }
   },
   target: 'node',
@@ -64,7 +64,7 @@ const config = {
         // It uses default OS directory by default. If you need
         // something more custom, pass a path to it.
         // I.e., babel?cacheDirectory=<path>
-        loaders: ['babel-loader?cacheDirectory'],
+        loaders: ['babel-loader?cacheDirectory']
         // Parse only app files! Without this it will go through
         // the entire project. In addition to being slow,
         // that will most likely result in an error.
